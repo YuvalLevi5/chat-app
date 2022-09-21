70a2c47198347baf539272ab09207f7d60005ba3
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-09-21 14:49:02 +0300

    chat page - start

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { allUsersRoute } from '../utils/ApiRoutes'
 
 const Chat = () => {
+  const navigate = useNavigate()
+  const [contacts, setContacts] = useState([])
+  const [currentUser, setCurrentUser] = useState(undefined)
+
+  useEffect(async () => {
+    if (!localStorage.getItem('chat-app-user')) {
+      navigate("/login");
+    } else {
+      setCurrentUser(await JSON.parse(localStorage.getItem('chat-app-user')));
+    }
+  }, [])
+
+  useEffect(async () => {
+    if (currentUser) {
+      if (currentUser.isAvatarImageSet) {
+        const data = await axios.get(`${allUsersRoute}/${currentUser._id}`)
+        setContacts(data.data)
+      } else {
+        navigate('/setAvatar')
+      }
+    }
+  }, [currentUser])
+
+
   return (
-    <div>Chat</div>
+    <>
+      <section className='chat-page'>
+        <div className="container">
+
+        </div>
+      </section>
+    </>
   )
 }
 
