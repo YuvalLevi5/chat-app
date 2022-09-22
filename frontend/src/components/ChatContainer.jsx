986d3e9bfd95200009805cbeb76f43f021b1b0d4
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-09-22 12:19:12 +0300

    chat container base

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
-const ChatContainer = () => {
+const ChatContainer = ({ currentChat }) => {
   return (
-    <div>ChatContainer</div>
+    <>
+      {
+        currentChat && (
+          <section>
+            <div className="chat-header">
+              <div className="user-details">
+                <div className="avatar">
+                  <img
+                    src={`data:image/svg+xml;base64,${currentChat.avatarImage}`}
+                    alt="avatar"
+                  />
+                </div>
+                <div className="username">
+                  <h3>{currentChat.username}</h3>
+                </div>
+              </div>
+            </div>
+            <div className="chat-messages"></div>
+            <div className="chat-input"></div>
+          </section >
+        )
+      }
+    </>
   )
 }
 
