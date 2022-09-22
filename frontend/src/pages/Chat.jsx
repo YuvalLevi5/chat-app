8c8b3b7cd7f75011d4838af960c91dafcc5c4568
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-09-22 12:10:00 +0300

    fix loading bug

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -4,19 +4,21 @@ import { useNavigate } from 'react-router-dom'
 import { allUsersRoute } from '../utils/ApiRoutes'
 import Contacts from '../components/Contacts'
 import Welcome from '../components/Welcome'
+import ChatContainer from '../components/ChatContainer'
 
 const Chat = () => {
   const navigate = useNavigate()
   const [contacts, setContacts] = useState([])
   const [currentUser, setCurrentUser] = useState(undefined)
   const [currentChat, setCurrentChat] = useState(undefined)
-
+  const [isLoaded, setIsLoaded] = useState(false)
   useEffect(() => {
     async function check() {
       if (!localStorage.getItem('chat-app-user')) {
         navigate("/login");
       } else {
         setCurrentUser(await JSON.parse(localStorage.getItem('chat-app-user')))
+        setIsLoaded(true)
       }
     }
     check()
@@ -46,7 +48,15 @@ const Chat = () => {
       <section className='chat-page'>
         <div className="container">
           <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
-          <Welcome currentUser={currentUser} />
+          {
+            isLoaded && currentChat === undefined ? (
+              <Welcome currentUser={currentUser} />
+            ) : (
+              <ChatContainer currentUser={currentUser} />
+            )
+
+          }
+
         </div>
       </section>
     </>
