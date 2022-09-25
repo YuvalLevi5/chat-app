2ae8198a44e03c55b97e6e6d439d73161f471d57
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-09-25 12:06:47 +0300

    logout btn

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -3,8 +3,15 @@ import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { BiPowerOff } from 'react-icons/bi'
 const Logout = () => {
+    const navigate = useNavigate()
+    const handleClick = async () => {
+        localStorage.clear()
+        navigate('/login')
+    }
     return (
-        <div>Logout</div>
+        <div onClick={handleClick} className='logout-section'>
+            <BiPowerOff />
+        </div>
     )
 }
 
